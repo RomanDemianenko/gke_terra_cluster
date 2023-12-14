851cd5789afd68a947c9235c913ebb0b4c1b0165
RomanDemianenko <romandemianenko192@gmail.com>
2023-12-14 14:00:46 +0000

    update source name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "gke_cluster" {
-  source         = "https://github.com/RomanDemianenko/gke_cluster"
+  source         = "https://github.com/RomanDemianenko/gke_terra_cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
